77b13a6e044ab52d0e41f982e33f45ea3535c484
saroff03 <106846859+saroff03@users.noreply.github.com>
2022-11-22 20:24:21 +0000

    add leafletjs map

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,21 @@ let fieldIp = document.getElementById("outcome-ip")
 let fieldLocation = document.getElementById("outcome-location")
 let fieldTimezone = document.getElementById("outcome-timezone")
 let fieldIsp = document.getElementById("outcome-isp")
+let lat = document.getElementById("lat")
+let long = document.getElementById("long")
+
+//LeaftetJS Map
+let myMap = L.map("map").setView([51.505, -0.09], 13);
+let marker = L.marker([51.5, -0.09]).addTo(myMap); 
+
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(myMap);
+
+
 
 function startFetch() {
   let ip = document.getElementById("ip-adress").value
@@ -18,6 +33,11 @@ function startFetch() {
     fieldLocation.innerHTML = response.location.city + `,` + response.location.country;
     fieldTimezone.innerHTML = `UTC ` + response.location.timezone;
     fieldIsp.innerHTML = response.isp;
+    lat.innerHTML = response.location.lat;
+    long.innerHTML = response.location.lng;
+
+    myMap.setView([lat.textContent, long.textContent]);
+    marker.setLatLng([lat.textContent, long.textContent]);
   })    
 
   .catch(err => {
@@ -27,4 +47,3 @@ function startFetch() {
 
 
 searchBtn.addEventListener("click", startFetch);
-
