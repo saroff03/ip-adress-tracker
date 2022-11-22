ae98f478176b93695a063dcf6b82cc9e8d843a32
saroff03 <106846859+saroff03@users.noreply.github.com>
2022-11-22 20:36:27 +0000

    add backend server to hide my API key

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,28 @@
-import { MY_API_KEY } from "./config.js";
-
 const searchBtn = document.getElementById("search-btn") 
 let fieldIp = document.getElementById("outcome-ip")
 let fieldLocation = document.getElementById("outcome-location")
 let fieldTimezone = document.getElementById("outcome-timezone")
 let fieldIsp = document.getElementById("outcome-isp")
+let lat = document.getElementById("lat")
+let long = document.getElementById("long")
+
+//LeaftetJS Map
+let myMap = L.map("map").setView([51.505, -0.09], 13);
+let marker = L.marker([51.5, -0.09]).addTo(myMap); 
+
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(myMap);
+
+
 
 function startFetch() {
   let ip = document.getElementById("ip-adress").value
 
-  fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${MY_API_KEY}&ipAddress=${ip}`) 
+  fetch(`/api?ipAddress=${ip}`) 
 
   .then(response => response.json())
 
@@ -20,6 +33,11 @@ function startFetch() {
     fieldLocation.innerHTML = response.location.city + `,` + response.location.country;
     fieldTimezone.innerHTML = `UTC ` + response.location.timezone;
     fieldIsp.innerHTML = response.isp;
+    lat.innerHTML = response.location.lat;
+    long.innerHTML = response.location.lng;
+
+    myMap.setView([lat.textContent, long.textContent]);
+    marker.setLatLng([lat.textContent, long.textContent]);
   })    
 
   .catch(err => {
